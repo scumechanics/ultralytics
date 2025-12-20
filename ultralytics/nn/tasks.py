3a9bc3934917eca2ba936edf4b1abfd3c6a13d2c
scumechanics <38198985+scumechanics@users.noreply.github.com>
2025-12-20 16:36:00 +0800

    Update tasks.py
    Signed-off-by: scumechanics <38198985+scumechanics@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -13,6 +13,9 @@ import torch.nn as nn
 from ultralytics.nn.autobackend import check_class_names
 from ultralytics.nn.modules import (
     DCAFE,
+    DCAFE_V2,
+    DCAFE_Lite,
+    DCAFE_Hybrid,
     EUCB,
     ESSamp,
     AIFI,
@@ -1626,6 +1629,10 @@ def parse_model(d, ch, verbose=True):
         elif m is DCAFE:
             args = [ch[f]]
             c2 = ch[f]
+            
+       elif m in {DCAFE_V2, DCAFE_Lite, DCAFE_Hybrid}:
+           c1, c2 = ch[f], args[0] if args else ch[f]
+           args = [c1]
 
         
         elif m in frozenset({HGStem, HGBlock}):
