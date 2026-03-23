0853aff91d22641d3eeccd5af9c89a8a05aa2de7
scumechanics <38198985+scumechanics@users.noreply.github.com>
2026-03-23 13:16:23 +0800

    Update tasks.py
    Signed-off-by: scumechanics <38198985+scumechanics@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -41,7 +41,7 @@ from ultralytics.nn.modules.SELA import *
 from ultralytics.nn.modules.DEMAttention import *
 from ultralytics.nn.modules.MLAttention import *
 from ultralytics.nn.modules.LCAttention import *
-
+from ultralytics.nn.modules.EPSA import *
 
 from ultralytics.nn.modules import (
     C3k2_DFF_1, 
@@ -1687,7 +1687,7 @@ def parse_model(d, ch, verbose=True):
             args = [c1]
 
 
-        elif m in {iEMA,CLCA,SECA,LGAM,DSAttention,DSLAM,SELA,DEMAttention,MLAttention,LCAttention}:
+        elif m in {iEMA,CLCA,SECA,LGAM,DSAttention,DSLAM,SELA,DEMAttention,MLAttention,LCAttention,EPSA}:
             c2 = ch[f]
             args = [c2,*args]
         
