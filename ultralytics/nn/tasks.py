945fe323b7905f7a7f827c9d165d6656f1efd7d8
scumechanics <38198985+scumechanics@users.noreply.github.com>
2026-04-04 10:11:47 +0800

    Update tasks.py
    Signed-off-by: scumechanics <38198985+scumechanics@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -69,6 +69,8 @@ from ultralytics.nn.modules.MANet import *
 
 # from ultralytics.nn.modules.RepNCSPELAN42 import *
 
+from ultralytics.nn.modules.Slimneck import GSConv, VoVGSCSP
+
 
 from ultralytics.nn.modules.GELAN import RepNCSPELAN4, SPPELAN
 
@@ -1644,7 +1646,8 @@ def parse_model(d, ch, verbose=True):
             C3k2_BFAM_2,
             C3k2_GhostModule,
             MANet,
-            RepNCSPELAN4, SPPELAN
+            RepNCSPELAN4, SPPELAN,
+            GSConv, VoVGSCSP
             # RepNCSPELAN4_high,
             # RepNCSPELAN4_low
             
